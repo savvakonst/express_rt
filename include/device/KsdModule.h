c5efdeb63883262a44d2522634ad662bdb418771
SVK <savvakonst@gmail.com>
2022-04-06 17:12:46 +0300

    "Device_ifs" developing

---
+++ include/device/KsdModule.h
@@ -0,0 +1,169 @@
+#ifndef EXRT_KSDMODULE_H
+#define EXRT_KSDMODULE_H
+
+#include <cstdint>
+#include <map>
+#include <utility>
+
+#include "device/ModuleStream_ifs.h"
+#include "device/Module_ifs.h"
+
+class TaskMapper : public HierarchicalData_ifs {
+   public:
+    enum StructType
+    {
+        value,
+        array,
+        map,
+    };
+    const StructType struct_type_;
+
+    TaskMapper(const TaskMapper &task_mapper);
+
+    TaskMapper(DataType type);
+
+    TaskMapper(size_t len, const TaskMapper &value);
+
+    typedef std::pair<std::string, TaskMapper> tuple_t;
+
+    explicit TaskMapper(std::vector<tuple_t> vecmap);
+
+    ~TaskMapper() override;
+
+    [[nodiscard]] bool isArray() const override { return struct_type_ == StructType::array; };
+
+    [[nodiscard]] bool isMap() const override { return struct_type_ == StructType::map; };
+
+    [[nodiscard]] bool isValue() const override { return struct_type_ == StructType::value; };
+
+    [[nodiscard]] Value getValue() const override {  // necessarry to add guard
+        return {ptr_, type_};
+    };
+
+    [[nodiscard]] std::vector<HierarchicalData_ifs *> getArray() const override;
+
+    [[nodiscard]] getMapReturn_t getMap() const override;
+
+    [[nodiscard]] HierarchicalData_ifs *getArrayUnit(size_t id) const override;
+
+    [[nodiscard]] HierarchicalData_ifs *getMapUnit(const std::string &id) const override;
+
+    [[maybe_unused]] bool setValue(const Value &data);
+
+    [[maybe_unused]] bool setValue(const std::string &data);
+
+    void setReferencePtr(void *ptr);
+
+   private:
+    size_t size_ = 0;
+    DataType type_ = DataType::none_v;
+    void *ptr_ = nullptr;
+    std::vector<TaskMapper> vector_;
+    std::vector<tuple_t> vecmap_;
+    std::map<std::string, HierarchicalData_ifs *> map_;
+};
+
+/*
+ *
+ *
+ *
+ */
+
+class ExtensionManager;
+
+class KSDModule : public Module_ifs {
+   protected:
+    explicit KSDModule(const TaskMapper &field_map) : field_map_(field_map) {}
+
+#pragma pack(1)
+    struct MODULE_HEADER {
+        uint32_t size;
+        uint32_t id;
+        uint8_t sub;
+        uint8_t slot;
+        uint16_t version;
+        uint16_t check_sum;
+        uint16_t flag;
+        uint16_t dimension;
+        uint16_t sync_mode;
+        uint8_t reserved[12];
+    };
+#pragma pack()
+
+    static const TaskMapper header_map_;
+
+    TaskMapper field_map_;
+
+    Module_ifs *parent_module_ = nullptr;
+
+    ModuleStream_ifs *ethernet_stream_ = nullptr;
+
+    [[nodiscard]] const TaskMapper *getBranch(const std::string &prop_path) const;
+
+   public:
+    [[nodiscard]] bool hasTransceiver() const override { return false; }
+
+    [[nodiscard]] EthernetSettings getSrcAddress() const override { return {}; }
+
+    [[nodiscard]] bool isAvailable() const override { return true; }
+
+    [[nodiscard]] std::string getModulePath(bool full_path) const override;
+
+    [[nodiscard]] Module_ifs *getParentModule() const override { return parent_module_; }
+
+    bool setParentModule(Module_ifs *parent_module) override {
+        if (parent_module_) return true;
+        parent_module_ = parent_module;
+        return true;
+    }
+
+    [[nodiscard]] std::list<Module_ifs *> getSubModulesFromPath(const std::string &prop_path) const override {
+        return {};
+    }
+
+    std::map<std::string, PrmBuffer_ifs *> getPrmBufferMap() override { return {}; }
+
+    [[nodiscard]] const DataSchema_ifs *getPropertySchema() const override {
+        // error_message_ = "The getPropertySchema function is not realised yet";
+        return nullptr;
+    }
+
+    [[nodiscard]] std::string printProperties(const std::string &indent) const override;
+
+    [[nodiscard]] const HierarchicalData_ifs *getProperty(const std::string &prop_path) const override;
+
+    [[nodiscard]] std::string getPropertyAsTxt(const std::string &prop_path) const override;
+
+    bool setProperty(const std::string &prop_path, const Value &value) override;
+
+    bool setProperty(const std::string &prop_path, const HierarchicalData_ifs *hierarchical_data) override {
+        return false;
+    }
+
+    bool setPropertyAsTxt(const std::string &prop_path, const std::string &value) override;
+
+    bool removeProperty(const std::string &prop_path) override { return false; }
+
+    [[nodiscard]] bool isChannelAvailable(const std::string &prop_path) const override;
+
+    [[nodiscard]] Value getChannelProperty(const std::string &prop_path, const std::string &type) const override {
+        return Value();
+    }
+
+    [[nodiscard]] std::vector<std::pair<std::string, Module_ifs *>> getSubModules() const override { return {}; }
+
+    ModuleStream_ifs *getModuleStream() { return ethernet_stream_; }
+
+    bool removeModuleStream() {
+        if (ethernet_stream_) {
+            delete ethernet_stream_;
+            ethernet_stream_ = nullptr;
+            return true;
+        }
+        return false;
+    }
+
+   protected:
+};
+
+#endif
---
+++ lib/device/KsdModule.cpp
@@ -0,0 +1,191 @@
+
+#include "device/KsdModule.h"
+
+#include <regex>
+
+/*
+ *
+ *
+ *
+ *
+ */
+
+TaskMapper::TaskMapper(const TaskMapper& task_mapper)  //
+    : type_(task_mapper.type_),
+      struct_type_(task_mapper.struct_type_),
+      vecmap_(task_mapper.vecmap_),
+      vector_(task_mapper.vector_),
+      ptr_(task_mapper.ptr_),
+      size_(task_mapper.size_) {
+    for (auto& i : vecmap_) {
+        map_[i.first] = &(i.second);
+    }
+}
+
+TaskMapper::TaskMapper(DataType type) : struct_type_(StructType::value), type_(type) { size_ = getTSize(type_); }
+
+TaskMapper::TaskMapper(size_t len, const TaskMapper& value) : struct_type_(StructType::array) {
+    vector_.reserve(len);
+    for (size_t i = 0; i < len; i++) {
+        vector_.push_back(value);
+    }
+    size_ = len * value.size_;
+}
+
+TaskMapper::TaskMapper(std::vector<tuple_t> vecmap) : vecmap_(std::move(vecmap)), struct_type_(StructType::map) {
+    size_ = 0;
+    for (auto& i : vecmap_) {
+        size_ += i.second.size_;
+        map_[i.first] = &(i.second);
+    }
+}
+
+TaskMapper::~TaskMapper() {}
+
+std::vector<HierarchicalData_ifs*> TaskMapper::getArray() const {
+    std::vector<HierarchicalData_ifs*> ret;
+
+    ret.reserve(vector_.size());
+    for (auto& i : vector_) {
+        ret.push_back((TaskMapper*)&i);
+    }
+    return ret;
+}
+
+TaskMapper::getMapReturn_t TaskMapper::getMap() const {
+    // std::map<std::string, HierarchicalData_ifs*> ret;
+    // for (auto& i : vecmap_) {
+    //     ret[i.first] = (HierarchicalData_ifs*)&i.second;
+    // }
+    getMapReturn_t ret;
+    for (auto& i : vecmap_) {
+        ret.push_back({i.first, (HierarchicalData_ifs*)&i.second});
+    }
+    return ret;
+}
+
+HierarchicalData_ifs* TaskMapper::getArrayUnit(size_t id) const {
+    if (id >= vector_.size()) return nullptr;
+    return (TaskMapper*)&(vector_[id]);
+}
+
+HierarchicalData_ifs* TaskMapper::getMapUnit(const std::string& id) const {
+    auto map_uint = map_.find(id);
+    if (map_uint != map_.end()) return map_uint->second;
+    return nullptr;
+}
+
+bool TaskMapper::setValue(const Value& data) {
+    if (isValue() && (data.type_ == type_)) {
+        memcpy(ptr_, &data.value_.u64, getTSize(type_));
+        return true;
+    }
+    return false;
+}
+
+bool TaskMapper::setValue(const std::string& data) {
+    if (isValue()) {
+        auto d = Value(data, type_);
+        if (!d) return false;
+        memcpy(ptr_, &d.value_.u64, getTSize(type_));
+        return true;
+    }
+    return false;
+}
+
+void TaskMapper::setReferencePtr(void* ptr) {
+    ptr_ = ptr;
+    char* p = (char*)ptr;
+    switch (struct_type_) {
+    case array:
+        for (auto& i : vector_) {
+            i.setReferencePtr(p);
+            p += i.size_;
+        }
+        break;
+    case map:
+        for (auto& i : vecmap_) {
+            i.second.setReferencePtr(p);
+            p += i.second.size_;
+        }
+        break;
+    default:
+        break;
+    }
+}
+
+/*
+ *
+ *
+ *
+ */
+
+std::string KSDModule::getModulePath(bool full_path) const {
+    auto slot = getProperty("header/slot")->getValue().value_.u64;
+    auto sub_slot = getProperty("header/sub")->getValue().value_.u64;
+    auto path = std::to_string(slot) + ((sub_slot == 0) ? "" : "." + std::to_string(sub_slot)) + "/" + getID();
+
+    if (parent_module_ && full_path) path = parent_module_->getModulePath(true) + "/" + path;
+    return path;
+}
+
+const TaskMapper* KSDModule::getBranch(const std::string& prop_path) const {
+    return (const TaskMapper*)::getBranch(&field_map_, prop_path);
+    /*
+    const std::regex separator("[^/]+");
+    const std::regex int_exp("[ ]*(\\+|\\-)?([0-9]+)");
+
+    auto begin = std::sregex_iterator(prop_path.begin(), prop_path.end(), separator);
+    auto end = std::sregex_iterator();
+
+    const HierarchicalData_ifs* tree = &field_map_;
+    for (std::sregex_iterator i = begin; i != end; ++i) {
+        std::string match_str = i->str();
+        tree =
+            std::regex_match(match_str, int_exp) ? tree->getArrayUnit(stoul(match_str)) : tree->getMapUnit(match_str);
+        if (tree == nullptr) return nullptr;
+    }
+    return (const TaskMapper*)tree;
+     */
+}
+
+std::string KSDModule::printProperties(const std::string& indent) const {
+    const TaskMapper* h_data = &field_map_;
+    return toString(h_data, indent + "  ");
+}
+
+const HierarchicalData_ifs* KSDModule::getProperty(const std::string& prop_path) const {
+    auto tree = (TaskMapper*)getBranch(prop_path);
+    // if (tree->isValue())
+    return tree;
+}
+
+std::string KSDModule::getPropertyAsTxt(const std::string& prop_path) const {
+    auto res = getProperty(prop_path);
+    if (res->isValue()) return res->getValue().asString();
+    return toString(res, "");
+}
+
+bool KSDModule::setProperty(const std::string& prop_path, const Value& value) {
+    auto tree = (TaskMapper*)getBranch(prop_path);
+    return tree->setValue(value);
+}
+
+bool KSDModule::setPropertyAsTxt(const std::string& prop_path, const std::string& value) {
+    auto tree = (TaskMapper*)getBranch(prop_path);
+    return tree->setValue(value);
+}
+
+bool KSDModule::isChannelAvailable(const std::string& prop_path) const { return false; }
+
+const TaskMapper KSDModule::header_map_ =  //
+    TaskMapper({{"size", u32},
+                {"id", u32},
+                {"sub", u8},
+                {"slot", u8},
+                {"version", u16},
+                {"checkSum", u16},
+                {"flag", u16},
+                {"dimension", u16},
+                {"syncMode", u16},
+                {"reserved", {12, u8}}});
